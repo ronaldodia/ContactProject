a0b6ac93661635b8c6c031e51e6fd39f739a8a83
alassanedia <univnktt@gmail.com>
2015-03-12 12:54:48 -0400

    supprimer le dossier packages

--- packages/ContactProject/ContactLibrairies.dart
+++
@@ -1,9 +0,0 @@
-library ContactLibrairies;
-
-import 'package:dartlero/dartlero.dart';
-import 'dart:html';
-import 'dart:convert';
-part 'model/ContactEntities.dart';
-part 'model/ContactModel.dart';
-part 'view/contact_table.dart'; 
-
